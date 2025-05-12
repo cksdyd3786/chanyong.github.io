a352bc1b6b7e8e44d06a0bf026e4c2e461a37f5b
cksdyd3786 <73522065+cksdyd3786@users.noreply.github.com>
2025-05-12 11:31:09 +0900

    Title: 정수 명령 처리 2, Time: 18ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250512/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int N;
+string command[10000];
+int A[10000];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> A[i];
+        }
+    }
+
+    queue<int> q;
+    for(int i=0; i<N; i++){
+        if(command[i]=="push"){
+            q.push(A[i]);
+        }
+        else if(command[i]=="pop"){
+            cout<< q.front()<< endl;
+            q.pop();
+        }
+        else if(command[i]=="size"){
+            cout<< q.size()<< endl;
+        }
+        else if(command[i]=="empty"){
+            cout<< q.empty()<< endl;
+        }
+        else if(command[i]=="front"){
+            cout<< q.front()<< endl;
+        }
+    }
+
+    return 0;
+}
