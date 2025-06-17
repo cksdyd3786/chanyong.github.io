05a7c0187d2ee1374904ce4526504d87ba79815e
cksdyd3786 <73522065+cksdyd3786@users.noreply.github.com>
2025-06-17 15:36:23 +0900

    Title: 정수 명령 처리, Time: 13ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250617/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int n;
+stack<int> s;
+
+
+int main() {
+
+    cin>> n;
+
+    for(int i=0; i<n; i++){
+        string command;
+        cin>> command;
+        
+        if(command == "push"){
+            int x;
+            cin>> x;
+            s.push(x);
+        }
+        else if(command == "pop"){
+            int v = s.top();
+            s.pop();
+            cout<< v << endl;
+        }
+        else if(command == "size"){
+            cout<< s.size()<< endl;
+        }
+        else if(command == "empty"){
+            if(s.empty())
+                cout<< "1"<< endl;
+            else   
+                cout<< "0"<< endl;
+        }
+        else{
+            cout<< s.top()<< endl;
+        }
+    }
+    
+    return 0;
+}
